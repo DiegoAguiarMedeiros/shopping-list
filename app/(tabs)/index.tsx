ec7372d53140fa2a5d66a3ebfba63c36a04baa55
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2023-03-24 20:34:41 -0300

    add fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,11 @@
 import { StyleSheet } from 'react-native';
 
 import Home from '../../screens/home';
-import { Text, View } from '../../components/Themed';
+import { View } from '../../components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One 2</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Home />
     </View>
   );
